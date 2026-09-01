4d634db62f39f3e80390ea527dbd5e0a4e5b2883
sowmiyatechleafe-cpu <sowmiyatechleafe@gmail.com>
2026-09-01 17:20:38 +0530

    create contactpage ui

--- techleafewebsite/src/router/index.tsx
+++ techleafewebsite/src/router/index.tsx
@@ -1,8 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "../pages/HomePage";
 import AboutPage from "../pages/AboutPage";
-import { ContactPage } from "@mui/icons-material";
+// import { ContactPage } from "@mui/icons-material";
 import MainLayout from "../layouts/MainLayout";
+import ContactPage from "../pages/ContactPage";
+
+
 
 export const router = createBrowserRouter([
   {
